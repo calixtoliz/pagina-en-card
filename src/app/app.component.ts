d3d3659b1f7f0ed099d476b537f0615f6a706a16
LIZBETH CASTILLO <lizcalixto@gmail.com>
2018-05-13 18:15:13 -0500

    pagina de fotos

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  subtitulo='December 15,2017'
+  titulo='Just a Standard Format Post.'
+  contenido='Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua... '
+  link1='Desing'
+  link2='Photography'
+  comillas='"'
+
+  titulo2='Good design is making something intelligible and memorable. Great design is making something memorable and meaningful.'
+  subtitulo2='December 15, 2017'
+
+  titulo3='10 Interesting Facts About Caffeine.'
+  contenido3='Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua... '
+  link3='Health'
+  subtitulo3="December 10,2017"
+
+  titulo4="No Sugar Oatmeal Cookies."
+  contenido4="Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua... "
+  link4=" Cooking Health "
+
+  titulo5="What Your Music Preference Says About You and Your Personality."
+  subtitulo4="December 10, 2017"
+  link5=" Music Lifestyle "
+  contenido5="Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua.."
+  subtitulo5="December 10, 2017"
+
+  titulo6="Visiting Theme Parks Improves Your Health."
+  contenido6="Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua... "
+  link6="Health Lifestyle"
+
+  titulo7="Key Benefits Of Family Photography."
+  subtitulo7="December 10, 2017"
+  link7= "Family Photography" 
+  contenido7="Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua.."
+ 
+  titulo8="Workspace Design Trends and Ideas."
+  subtitulo8="December 10, 2017"
+  link8=  "Work Management" 
+  contenido8="Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua.."
+ 
+  titulo9="Create Meaningful Family Moments."
+  subtitulo9="December 10, 2017"
+  link9=  " Family Relationship " 
+  contenido9="Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua.."
+ 
+  titulo10="Throwback To The Good Old Days."
+  subtitulo10="December 10, 2017"
+  link10=  "Lifestyle " 
+  contenido10="Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua.."
+ 
+
+  titulo11="Just Another Standard Format Post.."
+  subtitulo11="December 10, 2017"
+  link11=  "Design Photography" 
+  contenido11="Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua.."
+ 
+
+  titulo12="Planning Your First Trip to Sydney."
+  subtitulo12="December 10, 2017"
+  link12=  "Travel Vacation" 
+  contenido12="Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua.."
+ 
+
+}
